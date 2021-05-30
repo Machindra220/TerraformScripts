16b5d4625f296134fe0805d574cfe66f9446489c
Machindra <machindra220@gmail.com>
2021-05-30 18:08:03 +0530

    Terraform installtion code or script

---
+++ InstallTerraform.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Run me with superuser privileges
+sudo useradd -s /bin/bash -d /home/ashok/ -m -G root YourUserName
+echo 'YourUserName  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+cd /home/YourUserName/
+wget https://releases.hashicorp.com/terraform/0.15.4/terraform_0.15.4_linux_amd64.zip
+unzip terraform_0.15.4_linux_amd64.zip
+mkdir downloads
+mv terraform downloads/
+
+----------Above code can be run to install terraform on your linux system -----------------------------------
+---------------------------------  add terraform permanent path in bash_profile file in linux ----------------
+export PATH=$PATH:downloads/
+
+source ~/.bash_profile
